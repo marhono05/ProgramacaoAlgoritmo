fd8174287b492d586077f14fa8c44507c5954541
marhono05 <marcelo_honorio@outlook.com.br>
2025-08-05 20:46:40 -0300

    Add files via upload

---
+++ programacao/PrimeiraVariavel.java
@@ -0,0 +1,12 @@
+public class PrimeiraVariavel {
+
+    public static void main(String[] args) {
+        
+        String nome = "Marcelo Luiz";
+        int idade = 23;
+        double altura = 1.78;
+
+        System.out.println("Olá eu sou o " + nome + " tenho " + idade + " e tenho " + altura + " de altura");
+    }
+    
+}
---
+++ programacao/Soma.java
@@ -0,0 +1,11 @@
+public class Soma {
+    
+    public static void main(String[] args) {
+        
+        int n1 = 5;
+        int n2 = 3;
+        int soma = n1 + n2;
+
+        System.out.println("O valor da soma é " + soma);
+    }
+}
