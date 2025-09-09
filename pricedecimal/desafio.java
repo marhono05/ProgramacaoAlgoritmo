d1cdbf0c6c96d199ab84fcf9596ac9e8c92cc3fc
Marcelo Honório <marcelo_honorio@outlook.com.br>
2025-09-08 22:55:56 -0300

    Add files via upload
    Exemplo Decimal e Desafio da Tabela Price.

---
+++ pricedecimal/desafio.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class desafio {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        double prestacaofixa, valorfinanciamento, jurosmes, i, n, valorcima, resultcima, valorbaixo, resultbaixo;
+
+        System.out.println("Qual o valor do financiamento?");
+        valorfinanciamento = ler.nextDouble();
+
+        System.out.println("Qual a taxa de juros ao mês?");
+        jurosmes = ler.nextDouble(); 
+
+        System.out.println("Qual a quantidade de meses para pagar?");
+        n = ler.nextDouble();
+
+        i = jurosmes /100;
+
+       
+
+
+
+        
+
+
+
+        
+
+
+        
+
+        
+
+        
+
+      
+
+   
+
+
+
+     
+      
+
+
+
+
+
+       
+    }
+}
