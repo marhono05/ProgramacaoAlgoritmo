bc30d7ff17a474f1e6ad6c9315114615e5d0d301
Marcelo Honório <marcelo_honorio@outlook.com.br>
2025-10-28 20:48:04 -0300

    Estruturas de Repetição

---
+++ EstruturaRep/ExemploFor.java
@@ -0,0 +1,17 @@
+package EstruturaRep;
+
+public class ExemploFor {
+
+    public static void main(String[] args) {
+
+        int i;
+        // I de iterador
+
+
+        // (Valor inicial da variável; Valor final da variável; Incremento)
+        for(i=0; i<=9; i++){
+            System.out.println("Etec de Fernandópolis");
+        }
+    }
+    
+}
